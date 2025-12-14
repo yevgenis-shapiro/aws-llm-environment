ca84f7128bd3a3cc22ae993705e30db9a19bbf44
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-12-14 15:35:27 +0200

    update

--- modules/kind/resources.tf
+++ modules/kind/resources.tf
@@ -86,6 +86,13 @@ controller:
   service:
     type: "LoadBalancer"
   watchIngressWithoutClass: true
+  tolerations:
+  - key: "node-role.kubernetes.io/control-plane"
+    operator: "Exists"
+    effect: "NoSchedule"
+  # Optional: Use a node selector to *force* it onto the master node if others are available
+  nodeSelector:
+    node-role.kubernetes.io/control-plane: ""
   publishService:
     enabled: false
   extraArgs:
